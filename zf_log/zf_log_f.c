57346247b78c4b69a2b83ac84062e63fac5f3576
xiaoxing cheng <xiaoxingcheng@xiaoxings-Mini.lan>
2021-01-14 23:38:38 -0500

    add all level of log to the fortran interface

--- zf_log/zf_log_f.c
+++ zf_log/zf_log_f.c
@@ -1,5 +1,25 @@
 #include "zf_log/zf_log.h"
 
+void zf_logv_f(const char *text){
+    ZF_LOGV(text);
+}
+
 void zf_logd_f(const char *text){
     ZF_LOGD(text);
+}
+
+void zf_logi_f(const char *text){
+    ZF_LOGI(text);
+}
+
+void zf_logw_f(const char *text){
+    ZF_LOGW(text);
+}
+
+void zf_loge_f(const char *text){
+    ZF_LOGE(text);
+}
+
+void zf_logf_f(const char *text){
+    ZF_LOGF(text);
 }
